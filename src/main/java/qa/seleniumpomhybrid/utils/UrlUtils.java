3d9edd2edacbfa362e9fbe62e0873e03cb19c111
QA Jaldeep <37146361+QAJALDEEP@users.noreply.github.com>
2025-12-26 13:55:37 +0530

    Update UrlUtils.java

--- src/main/java/qa/seleniumpomhybrid/utils/UrlUtils.java
+++ src/main/java/qa/seleniumpomhybrid/utils/UrlUtils.java
@@ -2,6 +2,6 @@ package qa.seleniumpomhybrid.utils;
 
 public class UrlUtils {
 	
-	final String APPOINTMENT_URL = "https://dev-app.tryteddy.com/appointment";
+	final String APPOINTMENT_URL = "https://www.example.com/appointment";
 
 }
